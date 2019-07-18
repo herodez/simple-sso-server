bb3ae81c1d0e442b133ad1ec0a7e4b1cf13a451b
Mario Gonzalez <mgonzalez@optimeconsulting.com>
2019-07-18 09:21:06 -0500

    feat: Convert SecurityController to a service

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -2,10 +2,14 @@
 
 namespace Optime\SimpleSsoServerBundle\Controller;
 
+use Optime\SimpleSso\UseCase\CreateLoginOtpUseCase;
+use Optime\SimpleSso\UseCase\GetCredentialsUseCase;
+use Optime\SimpleSso\UseCase\VerifyActiveSessionUseCase;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -16,74 +20,71 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class SecurityController extends AbstractController
 {
-    public function __construct()
-    {
-    }
-    
     /**
-	 * @Route("/login",
-	 *  condition="request.query.has('username') and request.query.has('_target')"
-	 * )
-	 * @Method("GET")
-	 * @Security("is_authenticated()")
-	 * @param Request $request
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse
-	 */
-    public function loginAction(Request $request)
+     * @Route("/login",
+     *  condition="request.query.has('username') and request.query.has('_target')"
+     * )
+     * @Method("GET")
+     * @Security("is_authenticated()")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function loginAction(Request $request, CreateLoginOtpUseCase $useCase)
     {
-        $useCase = $this->get('simple_sso_server.use_case.create_login_otp');
-
         return $this->redirect($useCase->handle(
             $request->query->get('username'),
             $request->query->get('_target')
         ));
     }
-	
-	/**
-	 * @Route("/login")
-	 * @Method("POST")
-	 * @param Request $request
-	 * @return Response
-	 * @throws \Exception
-	 */
-    public function getCredentialsAction(Request $request)
-    {
+    
+    /**
+     * @Route("/login")
+     * @Method("POST")
+     * @param Request $request
+     * @return Response
+     * @throws \Exception
+     */
+    public function getCredentialsAction(
+        Request $request,
+        GetCredentialsUseCase $useCase,
+        ContainerInterface $container
+    ) {
         try {
-            $credentials = $this->get('simple_sso_server.use_case.get_credentials')->handle(
+            $credentials = $useCase->handle(
                 $request->get('otp'),
                 $request->get('created'),
                 $request->get('password')
             );
         } catch (\Exception $e) {
-            if ($this->container->has('logger')) {
-                $this->get('logger')->error($e->getMessage(), [
+            if ($container->has('logger')) {
+                $container->get('logger')->error($e->getMessage(), [
                     'line' => $e->getLine(),
                     'code' => $e->getCode(),
                     'file' => $e->getFile(),
                     'trace' => $e->getTraceAsString(),
                 ]);
             }
-
+            
             throw $e;
         }
-
+        
         return new Response(base64_encode($credentials));
     }
-	
-	/**
-	 * @Route("/verify")
-	 * @Method("GET")
-	 * @param Request $request
-	 * @return Response
-	 */
-    public function checkActiveSessionAction(Request $request)
+    
+    /**
+     * @Route("/verify")
+     * @Method("GET")
+     * @param Request $request
+     * @return Response
+     */
+    public function checkActiveSessionAction(Request $request, VerifyActiveSessionUseCase $useCase)
     {
-        $result = $this->get('simple_sso_server.use_case.verify_active_session')->handle(
+        $result = $useCase->handle(
             $request->query->get('otp'),
             $request->query->get('created'),
             $request->query->get('password')
         );
-
+        
         return new Response($result ? 1 : 0);
     }
 }
